2e2eef7b47b4e7d963afbee595681722a74a52f2
Anna Kucherenko <Annakucherenko1997@gmail.com>
2023-08-17 19:10:22 +0200

    feat: go back implemented

--- src/components/EstablishmentPage.tsx
+++ src/components/EstablishmentPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { getEstablishment } from '../api/ratingsAPI';
 import { useEffect, useState } from 'react';
 import { headerStyle } from './EstablishmentsTable';
@@ -8,8 +8,18 @@ import { logoStyle } from './HomePage';
 type EstablishmentsType = {
   [key: string]: any;
 };
+const buttonStyle = {
+  background: 'rgba(51, 51, 51, 0.9)',
+  padding: '10px',
+  marginTop: '5px',
+  marginLeft: '50px',
+  color: 'white',
+  borderRadius: '5px',
+};
+
 const EstablishmentPage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [error, setError] = useState<{
     message: string;
     [key: string]: string;
@@ -27,14 +37,17 @@ const EstablishmentPage = () => {
     );
   });
 
-  function formatDate(dateString: string) {
+  const formatDate = (dateString: string) => {
     const date = new Date(dateString);
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = String(date.getFullYear()).slice(-2);
 
     return `${day}/${month}/${year}`;
-  }
+  };
+  const handleBtnClick = () => {
+    navigate('/');
+  };
   if (error) {
     return <div>Error: {error.message}</div>;
   } else {
@@ -51,22 +64,36 @@ const EstablishmentPage = () => {
               </tr>
               <tr style={{ borderBottom: '1px solid #ccc' }}>
                 <td style={{ ...cellStyle, borderRight: '1px solid #ccc' }}>
-                  {establishment?.AddressLine1}<br />
-                  {establishment?.AddressLine2}<br />
-                  {establishment?.AddressLine3}<br />
-                  {establishment?.AddressLine4}
+                  {establishment?.AddressLine1 !== '' &&
+                    `${establishment?.AddressLine1},`}
+                  <br />
+                  {establishment?.AddressLine2 !== '' &&
+                    `${establishment?.AddressLine2},`}
+                  <br />
+                  {establishment?.AddressLine3 !== '' &&
+                    `${establishment?.AddressLine3},`}
+                  <br />
+                  {establishment?.AddressLine4 !== '' &&
+                    establishment?.AddressLine4}
                 </td>
-                <td style={{ ...cellStyle, borderRight: '1px solid #ccc' }}>{establishment?.RatingValue}</td>
+                <td style={{ ...cellStyle, borderRight: '1px solid #ccc' }}>
+                  {establishment?.RatingValue}
+                </td>
                 <td style={cellStyle}>
-                  {establishment?.RatingDate ? formatDate(establishment?.RatingDate) : ""}
+                  {establishment?.RatingDate
+                    ? formatDate(establishment?.RatingDate)
+                    : ''}
                 </td>
                 <td style={cellStyle}></td>
               </tr>
             </tbody>
           </table>
         </div>
+        <button onClick={handleBtnClick} style={buttonStyle}>
+          go back
+        </button>
       </div>
     );
-  };
+  }
 };
 export default EstablishmentPage;

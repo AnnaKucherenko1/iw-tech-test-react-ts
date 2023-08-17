b89667ff092a9a07fe21717781b136719310864d
Anna Kucherenko <Annakucherenko1997@gmail.com>
2023-08-17 09:32:57 +0200

    feat: onClick function added

--- src/components/EstablishmentsTableRow.tsx
+++ src/components/EstablishmentsTableRow.tsx
@@ -1,14 +1,20 @@
 export const EstablishmentsTableRow: React.FC<{
   establishment: { [key: string]: string } | null | undefined;
 }> = ({ establishment }) => {
-
   const cellStyle = {
     fontSize: '20px',
+    cursor: 'pointer',
+  };
+  const handleClick = () => {
+    console.log(establishment, 'this was clicked');
   };
 
   return (
     <tr>
-      <td style={cellStyle}>{establishment?.BusinessName}</td>
+      <td style={cellStyle} onClick={handleClick}
+      >
+        {establishment?.BusinessName}
+      </td>
       <td style={cellStyle}>{establishment?.RatingValue}</td>
     </tr>
   );
